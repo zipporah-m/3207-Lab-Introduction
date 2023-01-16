0da243c546d6cccdaa5deb451116c48cf53bbe68
gk-3207-TU <54708777+gk-3207-TU@users.noreply.github.com>
2023-01-15 22:22:40 -0500

    Add files via upload

---
+++ print_random.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main()
+{
+	int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
+}
+
